6611c1c49023c26dc24da190eb2236422580b5e0
Vladislav <44846794+vlad-velichko@users.noreply.github.com>
2024-06-06 10:32:29 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,18 @@ import com.codeborne.selenide.SelenideConfig;
 import com.codeborne.selenide.SelenideDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
+import java.io.File;
+import java.nio.file.Files;
+
 import static com.codeborne.selenide.Browsers.EDGE;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 import static java.util.Collections.singletonList;
 
 public class Main {
     public static void main(String[] args) {
+        File file = new File(System.getenv("LOCALAPPDATA") + "/Microsoft/WindowsApps/ms-teams.exe");
+        System.out.println("file.exists() = " + file.exists());
+        System.out.println("Files.exists(...) = " + Files.exists(file.toPath(), NOFOLLOW_LINKS));
         new SelenideDriver(new SelenideConfig()
                 .browser(EDGE)
                 .browserCapabilities(new EdgeOptions()
